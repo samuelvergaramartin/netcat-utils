256bd6553c114340df12aef88dd70353d9d318fe
Sam170703 <sam170703dev@gmail.com>
2023-07-27 17:58:04 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 exports.NetCatIntents = require('./Discord-Utils/javaScript/Build/IntentsBuilder');
-exports.NetCatIntentsDefault = require('./Discord-Utils/javaScript/Build/IntentsBuilder');
+exports.NetCatIntentsDefault = require('./Discord-Utils/javaScript/data/DefaultIntents');
+exports.NetCatIntentsAll = require('./Discord-Utils/javaScript/data/AllIntents');
