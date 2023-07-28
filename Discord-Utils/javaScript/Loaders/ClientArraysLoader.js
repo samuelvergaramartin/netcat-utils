0c5d16e02c01a1fa58fe2a0fdfa212f157f05ef0
Sam170703 <sam170703dev@gmail.com>
2023-07-28 12:32:01 +0200

    Updated ClientArraysLoader.js

---
+++ Discord-Utils/javaScript/Loaders/ClientArraysLoader.js
@@ -0,0 +1,11 @@
+const NetCatIntentsArray = [];
+
+const NetCatArraysLoader = {
+    name: "NetCatLoader",
+    description: "Cargador de Arrays del cliente",
+    loadIntents: async(Intent1, Intent2, Intent3, Intent4, Intent5, Intent6,
+        Intent7, Intent8, Intent9, Intent10, Intent11, Intent12, Intent13, 
+        Intent14, Intent15, Intent16, Intent17, Intent18, Intent19)=> {
+
+    }
+}
