35df52bccdd43fecee00a36a578adb4a66c92415
Sam170703 <sam170703dev@gmail.com>
2023-07-29 18:00:40 +0200

    Updated index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.NetCatIntents = require('./Discord-Utils/javaScript/Build/IntentsBuilder
 exports.NetCatIntentsDefault = require('./Discord-Utils/javaScript/data/DefaultIntents');
 exports.NetCatIntentsAll = require('./Discord-Utils/javaScript/data/AllIntents');
 exports.NetCatArraysLoader = require('./Discord-Utils/javaScript/Loaders/ClientArraysLoader');
+exports.NetCatIntentsArray = require('./Discord-Utils/javaScript/Loaders/ClientArraysLoader');
