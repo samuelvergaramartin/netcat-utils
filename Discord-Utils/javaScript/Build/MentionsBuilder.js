31483f6aaf0ae3d6bd63d790e2da46423489836e
Sam170703 <sam170703dev@gmail.com>
2023-07-30 15:10:28 +0200

    Created MentionsBuilder.js

---
+++ Discord-Utils/javaScript/Build/MentionsBuilder.js
@@ -0,0 +1,15 @@
+/**
+ * @constant {Mentions} - Creamos las Mentions
+ */
+const NetCatMentions = {
+   roles: "roles",
+   users: "users",
+   everyone: "everyone"
+}
+
+/**
+* @exports Mentions - Exportamos las Mentions
+* @default - Exportación por defecto.
+*/
+
+module.exports = NetCatMentions;
