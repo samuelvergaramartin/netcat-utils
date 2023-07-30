f9bfaf5b169fe272166895acfb24baf5d79eaf41
Sam170703 <sam170703dev@gmail.com>
2023-07-30 14:51:56 +0200

    Created PartialsBuilder.js

---
+++ Discord-Utils/javaScript/Build/PartialsBuilder.js
@@ -0,0 +1,19 @@
+/**
+ * @constant {Partials} - Creamos los Partials
+ */
+const NetCatPartials = {
+    User: 0,
+    Channel: 1,
+    GuildMember: 2,
+    Message: 3,
+    Reaction: 4,
+    GuildScheduledEvent: 5,
+    ThreadMember: 6
+}
+
+/**
+ * @exports Partials - Exportamos los Partials
+ * @default - Exportación por defecto.
+ */
+
+module.exports = NetCatPartials;
