f6620fcfa60b20ec8acb3e9d322245a55ac882ab
Sam170703 <sam170703dev@gmail.com>
2023-12-20 12:32:51 +0100

    Created Partials.d.ts

---
+++ Discord-Utils/TypeScript/Schemas/Partials.d.ts
@@ -0,0 +1,13 @@
+interface NetCatPartialsSchema {
+    NetCatPartials: {
+        User: number,
+        Channel: number,
+        GuildMember: number,
+        Message: number,
+        Reaction: number,
+        GuildSheduledEvent: number,
+        ThreadMember: number
+    }
+}
+
+export default NetCatPartialsSchema;
