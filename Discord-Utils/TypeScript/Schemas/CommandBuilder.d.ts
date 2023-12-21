3093f49934e47df5f3db4b57ea11182d873f4fdb
Sam170703 <sam170703dev@gmail.com>
2023-12-21 13:01:54 +0100

    Created CommandBuilder.d.ts

---
+++ Discord-Utils/TypeScript/Schemas/CommandBuilder.d.ts
@@ -0,0 +1,39 @@
+interface CommandBuilderSchema {
+    Command: {
+        type: {
+            slash: number,
+            user: number,
+            message: number
+        },
+        options: {
+            type: {
+                subcommand: number,
+                subcommandgroup: number,
+                string: number,
+                integer: number,
+                boolean: number,
+                user: number,
+                channel: number,
+                role: number,
+                mentionable: number,
+                number: number,
+                attachment: number,
+            },
+            channeltypes: {
+                guild_text: number,
+                dm: number,
+                guild_voice: number,
+                group_dm: number,
+                guild_category: number,
+                guild_announcement: number,
+                announcement_thread: number,
+                public_thread: number,
+                guild_stage_voice: number,
+                guild_directory: number,
+                guild_forum: number
+            }
+        }
+    }
+}
+
+export default CommandBuilderSchema;
